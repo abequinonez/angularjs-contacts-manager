1470636adb8cb71bf8d214f21c12ec46815b0a21
Abraham Quinonez <aquinonez.dev@gmail.com>
2018-07-22 18:44:10 -0700

    feat: Initial commit (Firebase setup)

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -1,5 +1,19 @@
 angular
-  .module('components.auth', [
-    'ui.router',
-    'firebase'
-  ]);
+  .module('components.auth', ['ui.router', 'firebase'])
+  .config(function($firebaseRefProvider) {
+    var config = {
+      apiKey: 'AIzaSyBqaCYCWF-ZpquLwodpg598NBuoVw5dzS4',
+      authDomain: 'angularjs-contacts-manager.firebaseapp.com',
+      databaseURL: 'https://angularjs-contacts-manager.firebaseio.com',
+      projectId: 'angularjs-contacts-manager',
+      storageBucket: 'angularjs-contacts-manager.appspot.com',
+      messagingSenderId: '27490833041'
+    };
+
+    $firebaseRefProvider.registerUrl({
+      default: config.databaseURL,
+      contacts: config.databaseURL + '/contacts'
+    });
+
+    firebase.initializeApp(config);
+  });
